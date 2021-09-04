79be502e9a5e5bb53c0da46e3fb0396971d0e0fe
Akib <Akib@DESKTOP-8SFKD39>
2021-09-04 10:54:34 -0400

    Add project files.

---
+++ Problem1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace HelloWorldApp
+{
+    class Problem1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q1 : Enter the string:");
+            string s = Console.ReadLine();
+            var first = s.Substring(0, (int)(s.Length / 2));
+            var last = s.Substring((int)(s.Length/2), (int)(s.Length/2));
+            Debug.WriteLine(first);
+        }
+    }
+}
