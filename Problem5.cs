61de7e44091cade77d5fc5ce94bfacea172a7032
Akib <Akib@DESKTOP-8SFKD39>
2021-09-04 11:03:07 -0400

    Update1.1.1.1

--- Problem5.cs
+++ Problem5.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Assignment_1
 {
     class Problem5
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q1 : Enter the string:");
+            string s = Console.ReadLine();
+            Debug.WriteLine(s);
+        }
     }
 }
